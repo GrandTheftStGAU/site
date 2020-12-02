c152eab0cf86b91f1a87ec6db47074ae5d46de2b
GrandTheftStGAU <75370882+GrandTheftStGAU@users.noreply.github.com>
2020-12-02 16:49:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById('myTopnav')
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	}
+	else {
+		x.className = "topnav";
+	}
+} 
+
+//Плавный якорь
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
